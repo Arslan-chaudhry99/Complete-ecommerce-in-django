070eafa693d3d52c2af55d12eeb7420930c3b7b7
Arslan_Chaudhry <arslanchaudhry9011@gmail.com>
2022-04-07 08:14:08 +0500

    Initial commit

--- nusmap/models.py
+++ nusmap/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    slug=models.CharField( max_length=150, null=False, blank=False)
+    name=models.CharField( max_length=150, null=False, blank=False)
+    imge=models.ImageField( upload_to=None)
