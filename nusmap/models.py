98134fc5b46d7e81d4881d7ee58e14e08fbc70a0
Arslan_Chaudhry <arslanchaudhry9011@gmail.com>
2022-04-07 08:26:01 +0500

    Initial commit

--- nusmap/models.py
+++ nusmap/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 # Create your models here.
 
+# def get_file_path(request, filname):
+
+
 class Category(models.Model):
     slug=models.CharField( max_length=150, null=False, blank=False)
     name=models.CharField( max_length=150, null=False, blank=False)
-    imge=models.ImageField( upload_to=None)
+    imge=models.ImageField( upload_to=get_file_path, null=True, blank=True)
--- nusmilepro/settings.py
+++ nusmilepro/settings.py
@@ -118,10 +118,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+#menual
+MEDIA_URL='/images/'
+
+MEDIA_ROOT= BASE_DIR / 'static'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-MEDIA_URL='/images/'
